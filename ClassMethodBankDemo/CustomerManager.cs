75971a5d7c76bcd2f89d7f012a2262b567d77dd7
mkilicarslan <mkilicarslankarapinar@gmail.com>
2022-08-29 15:11:46 +0300

    Add project files.

---
+++ ClassMethodBankDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodBankDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer has been added!");
+            Console.WriteLine(customer.FirstName + " " + customer.SecondName);
+            Console.WriteLine("-----------------------");
+            customers.Add(customer);
+        }
+        public void List()
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.FirstName + " " + customer.SecondName);
+            }
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.SecondName + " customer has been deleted!");
+            customers.Remove(customer);
+        }
+    }
+}
---
+++ ClassMethodBankDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMethodBankDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Kilicarslan";
+            customer1.SecondName = "Karapinar";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 1;
+            customer2.FirstName = "Buse";
+            customer2.SecondName = "Gültekin";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 2;
+            customer3.FirstName = "Nurdan";
+            customer3.SecondName = "Barlas";
+
+
+
+            CustomerManager manager = new CustomerManager();
+
+            manager.Add(customer1);
+            manager.Add(customer2);
+            manager.Add(customer3);
+
+            manager.List();
+            Console.WriteLine("-----------------------");
+            manager.Delete(customer2);
+            Console.WriteLine("-----------------------");
+            manager.List();
+        }
+    }
+}
